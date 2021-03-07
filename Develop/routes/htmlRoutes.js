fc9a126f4706094f6c0c50ed4855f4fad04e9d37
wildnei <wildneisuane@gmail.com>
2021-03-06 23:33:57 -0500

    adding ID's to the notes generated in our html

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,23 +1,22 @@
 
 const path = require('path');
 const fs = require('fs')
-const uuid = require('uuid')
-// const index = require('index')
+const { v4: uuidv4 } = require('uuid');
+const { json } = require('express');
 
-// ROUTING
 
 module.exports = (app) => {
 
+
+    // directs to notes page
     app.get('/notes', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/notes.html'));
     });
 
-    // If no matching route is found default to home
 
     app.get('/api/notes', (req, res) => {
         fs.readFile('./db/db.json', 'utf8', function (err, data) {
             res.send(data)
-            // console.log(__dirname);
         })
     })
 
@@ -25,9 +24,14 @@ module.exports = (app) => {
     app.post('/api/notes', (req, res) => {
         //1. capture our request body in newNote
         const newNote = req.body;
-        //1b. use uuid package to generate a new id string
-        //1c.use our new id string to add an 'id' property to our newNote
-        console.log(newNote);
+    
+        //1b. Read current array of notes
+        let genID = require('../db/db.json');
+        //1D. Generate a new ID using uuidv4()
+        newNote.id = genID.length + uuidv4()
+        //1D.use our new id string to add an 'id' property to our newNote
+        genID.push(newNote);
+
         try {
             //2. read db.json to get the current array of notes
             fs.readFile('./db/db.json', 'utf8', function (err, data) {
@@ -36,10 +40,10 @@ module.exports = (app) => {
                 }
                 //3. parse the string content of db.json into an array we can work with
                 const noteArray = JSON.parse(data);
-                console.log(noteArray);
-                //4. add our new note to th note array
+                
+                //4. add our new note to the note array
                 noteArray.push(newNote)
-                console.log(noteArray);
+          
                 //5. After we push our new note, write our note array back into db.json
                 fs.writeFile('./db/db.json', JSON.stringify(noteArray), (err) => {
                     if (err) {
@@ -51,12 +55,29 @@ module.exports = (app) => {
                 });
             });
         } catch (e) {
+            // if error executing show 500
             res.status(500).json(e);
         }
+
+        app.delete('/api/notes/:id', (req, res) => {
+            const selectById = req.params.id;
+            // Read current array 
+            let currentFile = require('../db/db.json');
+            // select by ID
+            currentFile.splice((selectById - 1), 1)
+            // Write a new file after removing the information
+            fs.writeFile('./db/db.json', JSON.stringify(currentFile), function (err) {
+                if (err) 
+                console.log(err);
+            });
+            res.json()
+        });
     });
 
-  
+    // If no matching route is found default to home
+
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/index.html'));
     });
 };
+
